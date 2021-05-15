6985434da225705988a5cd059ee9756f98190bd5
vinita <imvinitarajoria@gmail.com>
2021-05-15 13:11:47 +0530

    Persistence Abstrac class core Java 6

--- persistence/src/persistence/DataBasePersistence.java
+++ persistence/src/persistence/DataBasePersistence.java
@@ -4,7 +4,6 @@ public class DataBasePersistence extends Persistence{
 
 	@Override
 	public void persist() {
-		// TODO Auto-generated method stub
 		System.out.println("DataBasePersistence");
 
 	}
